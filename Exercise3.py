6aa211087af8aae3da9d02e0ae6fea3ce614b924
giovanni <egiovannicoro@gmail.com>
2023-12-18 14:23:01 -0500

    archivos eleminados

--- Exercise3.py
+++
@@ -1,9 +0,0 @@
-edad=int(input("Ingrese su edad :"))
-if 0<edad<100:
-  print("Edad correcta")
-  if edad>18:
-    print("Usted es mayor de edad")
-  else:
-    print("Usted es menor de edad")
-else:
-  print("Edad incorrecta")
