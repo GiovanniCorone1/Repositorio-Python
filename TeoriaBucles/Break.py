f6f4cfd84206a89586dcd451135c9971a8843431
giovanni <egiovannicoro@gmail.com>
2023-12-22 11:41:41 -0500

    subiendo break,continue,pass

---
+++ TeoriaBucles/Break.py
@@ -0,0 +1,10 @@
+"""Nos permite detener un bucle,la ejecución
+del programa continúa con la siguiente 
+instrucción después del bucle
+"""
+for i in range(15):
+  if i==11:
+    break
+  print(i)
+
+print("programa terminado")  
---
+++ TeoriaBucles/continue_y_pass.py
@@ -0,0 +1,15 @@
+#print("USO DEL CONTINUE")
+# for i in range(7,19):
+#   #no mostrar cuando aparece el numero 12
+#   if i==12:
+#     continue
+#   print(i)
+
+# print("USO DEL PASS")
+
+for i in range(12,19):
+  #el pass no permite mostrar el numero 15
+  if i==15:
+    pass
+  else:
+   print(i)
